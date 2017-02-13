8497d9fde993773f72dbee441357e1de80d429f0
Jon Karyo <jkaryo1@jhu.edu>
2017-02-13 15:44:26 -0500

    Date

--- app/src/main/java/com/example/drivelesson/MainActivity.java
+++ app/src/main/java/com/example/drivelesson/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.drivelesson;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Date date;
+    private TextView dateField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dateField = (TextView) findViewById(R.id.textView);
+    }
+
+    public void startClock(View view) {
+        date = new Date();
+        dateField.setText(android.text.format.DateFormat.format("M/d/yy", date));
+
     }
 }
